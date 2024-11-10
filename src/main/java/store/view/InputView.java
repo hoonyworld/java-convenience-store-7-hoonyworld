dd4ff4a1637d3f29a43c1a8e4b1ccac3f91ccba2
DongHoon Lee <dhl1924@naver.com>
2024-11-11 03:45:21 +0900

    feat(view): 상품 선택 입력 기능 구현
    - 콘솔로부터 상품명과 수량을 입력받는 기능 추가
    - 입력된 문자열을 ','와 '-'를 구분자로 파싱하여 ProductSelectionDTO 리스트로 변환

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,4 +1,8 @@
 package store.view;
 
+import java.util.List;
+import store.dto.ProductSelectionDTO;
+
 public interface InputView {
+    List<ProductSelectionDTO> readProductSelections();
 }
--- src/main/java/store/view/console/ConsoleInputView.java
+++ src/main/java/store/view/console/ConsoleInputView.java
@@ -1,6 +1,29 @@
 package store.view.console;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import store.dto.ProductSelectionDTO;
+import store.exception.ArgumentErrorMessage;
+import store.exception.StoreArgumentException;
+import store.util.Parser;
 import store.view.InputView;
 
 public class ConsoleInputView implements InputView {
+    private static final String DELIMITER_COMMA = ",";
+    private static final String DELIMITER_DASH = "-";
+
+    @Override
+    public List<ProductSelectionDTO> readProductSelections() {
+        String input = Console.readLine();
+
+        if (input.isBlank()) {
+            throw StoreArgumentException.from(ArgumentErrorMessage.INVALID_INPUT);
+        }
+
+        return Stream.of(input.split(DELIMITER_COMMA))
+                .map(selection -> Parser.parseSelection(selection, DELIMITER_DASH))
+                .collect(Collectors.toList());
+    }
 }

3ab5a0c2b6166255936411d90d741fe1dfe3a7fb
DongHoon Lee <dhl1924@naver.com>
2024-11-11 02:10:00 +0900

    feat(controller): 상품 출력 로직 구현

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,5 +1,41 @@
 package store.controller;
 
-public class StoreController {
+import java.util.List;
+import store.dto.ProductDTO;
+import store.dto.ProductDisplayDTO;
+import store.service.StoreService;
+import store.template.RetryTemplate;
+import store.view.InputView;
+import store.view.OutputView;
 
+public class StoreController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final StoreService storeService;
+    private final RetryTemplate retryTemplate;
+
+    public StoreController(InputView inputView, OutputView outputView, StoreService storeService, RetryTemplate retryTemplate) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.storeService = storeService;
+        this.retryTemplate = retryTemplate;
+    }
+
+    public void run() {
+        handleUserInput();
+    }
+
+    private void handleUserInput() {
+        retryTemplate.execute(() -> {
+            displayProducts();
+//            String userInput = inputView.readUserInput();
+//            processUserInput(userInput);
+        });
+    }
+
+    private void displayProducts() {
+        List<ProductDTO> productDTOS = storeService.findAllProducts();
+        List<ProductDisplayDTO> productDisplayDTOS = storeService.findAllProductsForDisplay(productDTOS);
+        outputView.displayProductCatalog(productDisplayDTOS);
+    }
 }

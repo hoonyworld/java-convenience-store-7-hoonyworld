09b7ccea5bf5c1ad288bcb358f4e2b50d4a716c0
DongHoon Lee <dhl1924@naver.com>
2024-11-10 21:37:37 +0900

    feat(config): controller, service, view, template 의존성 주입 메서드 구현

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,12 +1,17 @@
 package store;
 
 import store.config.AppConfig;
+import store.controller.StoreController;
 import store.io.CacheFileInitializer;
 
 public class Application {
     public static void main(String[] args) {
         AppConfig appConfig = AppConfig.getInstance();
+
         CacheFileInitializer cacheFileInitializer = appConfig.cacheFileInitializer();
         cacheFileInitializer.initializeProductsCacheFile();
+
+        StoreController storeController = appConfig.storeController();
+        storeController.run();
     }
 }
--- src/main/java/store/config/AppConfig.java
+++ src/main/java/store/config/AppConfig.java
@@ -1,7 +1,14 @@
 package store.config;
 
 import java.nio.file.Path;
+import store.controller.StoreController;
 import store.io.CacheFileInitializer;
+import store.service.StoreService;
+import store.template.RetryTemplate;
+import store.view.InputView;
+import store.view.OutputView;
+import store.view.console.ConsoleInputView;
+import store.view.console.ConsoleOutputView;
 
 public class AppConfig {
     private static final Path PRODUCTS_FILE_PATH = Path.of("src/main/resources/products.md");
@@ -20,5 +27,25 @@ public class AppConfig {
 
     public CacheFileInitializer cacheFileInitializer() {
         return new CacheFileInitializer(PRODUCTS_FILE_PATH, PRODUCTS_CACHE_FILE_PATH);
+    }
+
+    public StoreController storeController() {
+        return new StoreController(createInputView(), createOutputView(), createStoreService(), createRetryTemplate());
+    }
+
+    private InputView createInputView() {
+        return new ConsoleInputView();
+    }
+
+    private OutputView createOutputView() {
+        return new ConsoleOutputView();
+    }
+
+    private StoreService createStoreService() {
+        return new StoreService();
+    }
+
+    private RetryTemplate createRetryTemplate() {
+        return new RetryTemplate(new ConsoleOutputView());
     }
 }

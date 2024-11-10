5a29275d42b28655b03f78e8104efc15e9af095e
DongHoon Lee <dhl1924@naver.com>
2024-11-11 00:40:12 +0900

    feat(exception): VO에 대한 예외 메시지 추가

--- src/main/java/store/exception/ArgumentErrorMessage.java
+++ src/main/java/store/exception/ArgumentErrorMessage.java
@@ -1,7 +1,9 @@
 package store.exception;
 
 public enum ArgumentErrorMessage implements ExceptionType {
-    ;
+    INVALID_MONEY("금액은 0 이상의 값이어야 합니다."),
+    INVALID_NAME("이름은 null이거나 빈 값일 수 없습니다."),
+    INVALID_QUANTITY("수량은 0보다 크거나 같아야 합니다.");
 
     private final String message;
 

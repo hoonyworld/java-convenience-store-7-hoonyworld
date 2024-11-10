3e85fab7e1edaf80f7a067e966cca63f63e85e95
DongHoon Lee <dhl1924@naver.com>
2024-11-11 00:31:10 +0900

    feat(Quantity): Quantity VO 구현 및 예외 처리 추가

---
+++ src/main/java/store/domain/vo/Quantity.java
@@ -0,0 +1,49 @@
+package store.domain.vo;
+
+import java.util.Objects;
+import store.exception.ArgumentErrorMessage;
+import store.exception.StoreArgumentException;
+
+public class Quantity {
+    private final int amount;
+
+    private Quantity(int amount) {
+        validate(amount);
+        this.amount = amount;
+    }
+
+    public static Quantity newInstance(int amount) {
+        return new Quantity(amount);
+    }
+
+    public boolean isZero() {
+        return amount == 0;
+    }
+
+    private void validate(int amount) {
+        if (amount < 0) {
+            throw StoreArgumentException.from(ArgumentErrorMessage.INVALID_QUANTITY);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Quantity quantity)) {
+            return false;
+        }
+        return amount == quantity.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+
+    @Override
+    public String toString() {
+        return amount + "개";
+    }
+}
